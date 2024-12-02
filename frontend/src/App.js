c550694ad7f5ce29427e902f347ea949c61cc25a
Vaibhav Gajendra Adhav <154625347+vaibhavadhav84@users.noreply.github.com>
2024-12-03 00:35:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
         { name: 'street', type: 'text', label: 'Street', required: true },
         { name: 'city', type: 'text', label: 'City', required: true },
         { name: 'state', type: 'dropdown', label: 'State', options: ['California', 'Texas', 'New York'], required: true },
-        { name: 'zipCode', type: 'text', label: 'Zip Code', required: true },
+        { name: 'zipCode', type: 'text', label: 'Zip Code', required: false },
       ],
     },
     paymentInformation: {
